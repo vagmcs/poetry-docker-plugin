77969c3242fa40667663ca46dde4423ea137cbbb
Evangelos Michelioudakis <vagmcs@gmail.com>
2022-10-14 18:39:23 +0300

    📝 docs: Adds docstrings for instructions
    - Changes entrypoint command to use exec instead of shell.
    - Adds initial support for volumes.

--- poetry_docker_plugin/plugin.py
+++ poetry_docker_plugin/plugin.py
@@ -1,5 +1,5 @@
 # Types
-from typing import Any, Dict, Optional
+from typing import Any
 
 # Standard Library
 import re
@@ -14,13 +14,10 @@ from .docker_builder import *
 
 class DockerBuild(Command):
     name = "docker"
-    description = "Build docker image"
+    description = "Builds docker image."
 
     def handle(self) -> int:
-        # self.line(self.application.poetry.pyproject.data["tool"]["docker"])
-
         pyproject_config = self.application.poetry.pyproject.data
-
         config: Optional[Dict[str, Any]] = pyproject_config.get("tool", dict()).get("docker")
 
         if config is None:
@@ -28,9 +25,8 @@ class DockerBuild(Command):
             self.io.write_error_line(message)
             raise RuntimeError(message)
 
-        # TODO: cannot be empty and should follow pattern!
         image_name = config.get("image_name")
-        if image_name is None:
+        if image_name is None or re.search(".*/.*?(:.*)", image_name) is None:
             org: str = (
                 re.match("(.*)<.*>", pyproject_config.get("tool").get("poetry").get("authors")[0])
                 .group(1)
@@ -42,14 +38,15 @@ class DockerBuild(Command):
             image_name = f"{org}/{name}:latest"
             self.io.write_line(f"Image name is not defined, using '{image_name}'.")
 
+        # Create docker file
         docker_file = DockerFile(self.io)
 
-        # Append all docker arguments
+        # Append all docker ARG
         args = config.get("args", dict())
         for arg, default_value in args.items():
             docker_file.add(Arg(arg, default_value))
 
-        # Append from command
+        # Append FROM command
         base_image: Optional[str] = config.get("from")
         if base_image is None:
             message = "<b>poetry-docker-plugin</b>: No from statement found in [tool.docker] in pyproject.toml"
@@ -58,26 +55,37 @@ class DockerBuild(Command):
         else:
             docker_file.add(From(base_image))
 
-        # Append all docker labels
+        # Append all docker LABEL
         labels: Dict[str, str] = config.get("labels", dict())
         docker_file.add(Labels(labels))
 
-        # Append copy commands
+        # Append COPY commands
         copy_statements: List[Dict[str, str]] = config.get("copy", dict())
         for statement in copy_statements:
+
+            if "source" not in statement or "target" not in statement:
+                message = f"<b>poetry-docker-plugin</b>: Source/target not present in copy command '{str(statement)}'"
+                self.io.write_error_line(message)
+                raise RuntimeError(message)
+
             docker_file.add(Copy(statement.get("source"), statement.get("target")))
-            # TODO: should check if the dict has another format!
 
+        # Append ENV commands
         env = config.get("env", dict())
-        for name, value in env.items():
-            docker_file.add(Env(name, value))
+        for env_name, value in env.items():
+            docker_file.add(Env(env_name, value))
 
+        # Append EXPOSE command
         ports = config.get("expose", list())
         for port in ports:
             docker_file.add(Expose(port))
 
-        # volumes = config.get("volume", list())
+        # Append VOLUME commands
+        volumes = config.get("volume", list())
+        for vol in volumes:
+            docker_file.add(Volume(vol))
 
+        # Append WORKDIR, USER, and RUN commands
         flow = config.get("flow", list())
         for instruction in flow:
             if "work_dir" in instruction:
@@ -89,13 +97,15 @@ class DockerBuild(Command):
             else:
                 self.io.write_error_line(f"Unknown command '{instruction}'")
 
+        # Append CMD command
         cmd = config.get("cmd")
         if cmd is not None:
             docker_file.add(Cmd(list(cmd)))
 
+        # Append ENTRYPOINT command
         entry_point = config.get("entrypoint")
         if entry_point is not None:
-            docker_file.add(EntryPoint(entry_point))
+            docker_file.add(EntryPoint(list(entry_point)))
 
         docker_file.build(image_name)
         return 0
@@ -107,5 +117,4 @@ def factory():
 
 class DockerPlugin(ApplicationPlugin):
     def activate(self, application: Application) -> None:
-        # print(application.poetry.pyproject.data)
         application.command_loader.register_factory("docker", factory)
