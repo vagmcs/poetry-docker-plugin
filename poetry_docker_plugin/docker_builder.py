443a9dd54edfeee4f3e0726d8547330b838c17bf
Evangelos Michelioudakis <vagmcs@gmail.com>
2022-10-14 18:39:23 +0300

    docs: adds docstrings for instructions
    - Changes entrypoint command to use exec instead of shell.
    - Adds initial support for volumes.

--- poetry_docker_plugin/docker_builder.py
+++ poetry_docker_plugin/docker_builder.py
@@ -11,36 +11,78 @@ from cleo.io.io import IO
 
 
 class Instruction(metaclass=abc.ABCMeta):
+    """
+    Docker instruction metaclass is extended by every docker command that
+    needs to be supported by the plugin.
+    """
+
     pass
 
 
 class Arg(Instruction):
-    def __init__(self, name: str, default_value: Optional[str] = None):
-        self._name = name
+    def __init__(self, arg_name: str, default_value: Optional[str] = None):
+        """
+        Creates a docker ARG instruction:
+
+        https://docs.docker.com/engine/reference/builder/#arg
+
+        :param arg_name: the argument name
+        :param default_value: a default value (optional)
+        """
+        self._arg_name = arg_name
         self._default_value = default_value
 
     def __str__(self):
-        return f"ARG {self._name}={self._default_value}" if self._default_value else f"ARG {self._name}"
+        return f"ARG {self._arg_name}={self._default_value}" if self._default_value else f"ARG {self._arg_name}"
 
 
 class Labels(Instruction):
     def __init__(self, labels: Dict[str, str]):
+        """
+        Creates a docker LABEL instruction:
+
+        https://docs.docker.com/engine/reference/builder/#label
+
+        :param labels: a dictionary of key/value labels
+        """
         self._labels = labels
 
     def __str__(self):
-        return f"LABEL {str(self._labels).replace(': ', '=').replace('{', '').replace('}', '')}"
+        return "\n".join([f"LABEL {key}={value}" for key, value in self._labels.items()])
 
 
 class From(Instruction):
-    def __init__(self, base_image: str):
+    def __init__(self, base_image: str, platform: Optional[str] = None):
+        """
+        Creates a docker FROM instruction:
+
+        https://docs.docker.com/engine/reference/builder/#from
+
+        :param base_image: the base image tag
+        :param platform: the target platform (optional)
+        """
         self._base_image = base_image
+        self._platform = platform
 
     def __str__(self):
-        return f"FROM {self._base_image}"
+        return (
+            f"FROM {self._base_image}"
+            if self._platform is None
+            else f"FROM --platform {self._platform} {self._base_image}"
+        )
 
 
 class Copy(Instruction):
     def __init__(self, source: str, destination: str):
+        """
+        Creates a docker COPY instruction:
+
+        https://docs.docker.com/engine/reference/builder/#copy
+
+        :param source: the source file to copy
+        :param destination: the destination inside docker container
+        """
+
         self._source = source
         self._destination = destination
 
@@ -49,16 +91,34 @@ class Copy(Instruction):
 
 
 class Env(Instruction):
-    def __init__(self, name: str, value: str):
-        self._name = name
+    def __init__(self, env_name: str, value: str):
+        """
+        Creates a docker ENV instruction:
+
+        https://docs.docker.com/engine/reference/builder/#env
+
+        :param env_name: the name of the environment variable
+        :param value: the value assigned to the variable
+        """
+        self._env_name = env_name
         self._value = value
 
     def __str__(self):
-        return f'ENV {self._name}="{self._value}"'
+        return f'ENV {self._env_name}="{self._value}"'
 
 
 class Expose(Instruction):
     def __init__(self, port: int):
+        """
+        Creates a docker EXPOSE instruction:
+
+        https://docs.docker.com/engine/reference/builder/#expose
+
+        :param port: a port to expose
+        """
+        if port < 0:
+            raise RuntimeError("Port numbers cannot be negative.")
+
         self._port = port
 
     def __str__(self):
@@ -67,6 +127,13 @@ class Expose(Instruction):
 
 class Volume(Instruction):
     def __init__(self, path: str):
+        """
+        Creates a docker VOLUME instruction:
+
+        https://docs.docker.com/engine/reference/builder/#volume
+
+        :param path: path to the volume location
+        """
         self._path = path
 
     def __str__(self):
@@ -75,6 +142,13 @@ class Volume(Instruction):
 
 class WorkDir(Instruction):
     def __init__(self, path: str):
+        """
+        Creates a docker WORKDIR instruction:
+
+        https://docs.docker.com/engine/reference/builder/#workdir
+
+        :param path: working directory path inside the container
+        """
         self._path = path
 
     def __str__(self):
@@ -82,15 +156,35 @@ class WorkDir(Instruction):
 
 
 class User(Instruction):
-    def __init__(self, user: str):
+    def __init__(self, user: str, group: Optional[str] = None):
+        """
+        Creates a docker USER instruction:
+
+        https://docs.docker.com/engine/reference/builder/#user
+
+        :param user: a default user for the remainder of the stage
+        :param group: a default group for the user
+        """
         self._user = user
+        self._group = group
 
     def __str__(self):
-        return f"USER {self._user}"
+        return f"USER {self._user}" if self._group is None else f"USER {self._user}:{self._group}"
 
 
 class Run(Instruction):
     def __init__(self, command: str):
+        """
+        Creates a docker RUN instruction:
+
+        https://docs.docker.com/engine/reference/builder/#run
+
+        Only shell form commands are supported in RUN instructions because they
+        inherit environment variables from the shell, and allow sub commands,
+        piping output, chaining commands, and I/O redirection.
+
+        :param command: a shell command to run
+        """
         self._command = command
 
     def __str__(self):
@@ -99,6 +193,17 @@ class Run(Instruction):
 
 class Cmd(Instruction):
     def __init__(self, args: List[str]):
+        """
+        Creates a docker CMD instruction:
+
+        https://docs.docker.com/engine/reference/builder/#cmd
+
+        Only exec form commands are supported in CMD instructions because most
+        shells do not forward process signals to child processes, which means
+        the SIGINT generated by pressing CTRL-C may not stop a child process.
+
+        :param args: a list of commands, arguments and parameters
+        """
         self._args = args
 
     def __str__(self):
@@ -106,22 +211,48 @@ class Cmd(Instruction):
 
 
 class EntryPoint(Instruction):
-    def __init__(self, command: str):
-        self._command = command
+    def __init__(self, args: List[str]):
+        """
+        Creates a docker ENTRYPOINT instruction:
+
+        https://docs.docker.com/engine/reference/builder/#entrypoint
+
+        Only exec form commands are supported in CMD instructions because most
+        shells do not forward process signals to child processes, which means
+        the SIGINT generated by pressing CTRL-C may not stop a child process.
+
+        :param args: a list of commands, arguments and parameters
+        """
+        self._args = args
 
     def __str__(self):
-        return f"ENTRYPOINT {self._command}"
+        return f"ENTRYPOINT {str(self._args)}"
 
 
 class DockerFile(object):
     def __init__(self, io: IO, instructions: Optional[List[Instruction]] = None):
+        """
+        Creates a docker file from a sequence of instructions.
+
+        :param instructions: a list of instructions to pre-append (optional)
+        """
         self._io = io
         self._instructions = [] if instructions is None else instructions
 
     def add(self, instruction: Instruction) -> None:
+        """
+        Adds a given docker instruction to the build.
+
+        :param instruction: a docker instruction
+        """
         self._instructions.append(instruction)
 
     def build(self, image_name: str) -> None:
+        """
+        Builds the docker image.
+
+        :param image_name: a name for the docker image
+        """
         if not os.path.exists("dist"):
             os.makedirs("dist")
 
